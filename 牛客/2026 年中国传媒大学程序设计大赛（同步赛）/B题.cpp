b9040df6ff7f2d0e3d07f44edc95624ee7d6fe07
Patchouli_Taki <3235253151@qq.com>
2026-03-21 23:22:15 +0800

    Add solution for B题
    Implement a solution for the simplified jumping stairs problem.

---
+++ 牛客/2026 年中国传媒大学程序设计大赛（同步赛）/B题.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio();
+	cin.tie(nullptr);
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		cout<<n/2+1<<'\n';
+	}	
+}//跳台阶问题简化版，每次将2个1合并成2即可
