9dbde5bfe24748b507ae383eeccc4bd8633ce201
Patchouli_Taki <3235253151@qq.com>
2026-03-22 20:30:25 +0800

    Add solution for B题

---
+++ 牛客/牛客周赛 Round 136/B题.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+    	int n;
+    	cin>>n;
+    	if(n==1)cout<<"a";
+    	else cout<<"No";
+    	cout<<'\n';
+    }
+    return 0;
+}
+// 回文串和不同字符，矛盾！
