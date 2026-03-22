843440ae833e05b0cbeb61480a4985275e54e7cb
Patchouli_Taki <3235253151@qq.com>
2026-03-22 20:29:04 +0800

    Add solution for A题

---
+++ 牛客/牛客周赛 Round 136/A题.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<b<<" "<<c<<" "<<a;
+    return 0;
+}
