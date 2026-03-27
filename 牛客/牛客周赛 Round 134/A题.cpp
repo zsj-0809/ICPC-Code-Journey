826d0550d1f16c25c6a146fa4e49ba0c6ac6db5b
Patchouli_Taki <3235253151@qq.com>
2026-03-27 22:21:09 +0800

    Add solution for A题
    Implement a program to determine rated status based on score.

---
+++ 牛客/牛客周赛 Round 134/A题.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); 
+    int s;
+    cin>>s;
+    if(s>=1600)cout<<"Unrated";
+    else cout<<"Rated";
+    return 0;
+}
