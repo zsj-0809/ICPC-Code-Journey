6361ea6398041e907efdf80b44f1bd2ffa245b0d
Patchouli_Taki <3235253151@qq.com>
2026-03-30 21:09:43 +0800

    Add solution for A题
    Implement a simple program that reads an integer and prints its value incrementally.

---
+++ 牛客/牛客周赛 Round 126/A题.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a;
+    cin>>a;
+    cout<<a<<" ";
+    a++;
+     cout<<a<<" ";
+    a++;
+     cout<<a<<" ";
+    a++;
+     cout<<a<<" ";
+    a++;
+    cout<<a<<" ";
+    
+    return 0;
+}
