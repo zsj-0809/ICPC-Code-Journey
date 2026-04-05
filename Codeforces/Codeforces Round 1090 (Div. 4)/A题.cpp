6c50ea84be96ef795667143de995f04b10156562
Patchouli_Taki <3235253151@qq.com>
2026-04-05 22:25:45 +0800

    Add solution for A题
    Implement a solution for Codeforces Round 1090 A题.

---
+++ Codeforces/Codeforces Round 1090 (Div. 4)/A题.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        cout<<x<<'\n';
+    }
+    return 0;
+}
+/*
+答案一定不会小于x，直接输出自身即可
+*/
