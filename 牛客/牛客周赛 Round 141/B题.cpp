de0a984ec25749c26fbb9cad8d0f5c976db3cf52
Patchouli_Taki <3235253151@qq.com>
2026-04-26 20:37:29 +0800

    Add solution for B题
    Implement solution for 牛客周赛 Round 141 B题.

---
+++ 牛客/牛客周赛 Round 141/B题.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+    	  long long x,y;
+    	  cin>>x>>y;
+    	  cout<<y<<'\n';
+	}
+    return 0;
+}
+/*
+任何数与自己异或为0 
+*/ 
