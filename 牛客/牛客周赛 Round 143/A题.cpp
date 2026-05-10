6145585157c73a2a81c563ccf42a3925886e99fe
Patchouli_Taki <3235253151@qq.com>
2026-05-10 23:35:02 +0800

    Add solution for A题

---
+++ 牛客/牛客周赛 Round 143/A题.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long x;
+    cin>>x;
+    cout<<1<<" "<<x;
+    return 0;
+}
